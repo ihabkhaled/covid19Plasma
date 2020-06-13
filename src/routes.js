8c79d8922f79b622309dd090cbfe2e86b6c24273
Ihab Khaled <i.khaled@eseed.net>
2020-06-13 13:36:20 +0200

    updates

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,16 @@
 
 import Dashboard from "views/Dashboard.jsx";
-import UserProfile from "views/UserProfile.jsx";
-import Login from "components/Login/Login";
+import TableList from "views/TableList.jsx";
+import Notifications from "views/Notifications.jsx";
 import Donor from "components/Donor/DonorForm.jsx";
-import Maps from "views/Maps";
 
 const dashboardRoutes = [
-  {
-    path: "/login",
-    name: "Login",
-    icon: "pe-7s-graph",
-    component: Login,
-    layout: "",
-    ignore:true
-  },
+
   {
     path: "/dashboard",
     name: "Dashboard",
     icon: "pe-7s-graph",
     component: Dashboard,
-    layout: ""
-  },
-  {
-    path: "/user",
-    name: "User Profile",
-    icon: "pe-7s-user",
-    component: UserProfile,
     layout: ""
   },
   {
@@ -37,48 +22,19 @@ const dashboardRoutes = [
     // ignore:true
   },
   {
-    path: "/maps",
-    name: "Maps",
-    icon: "pe-7s-user",
-    component: Maps,
-    layout: "",
-    // ignore:true
+    path: "/table",
+    name: "Table List",
+    icon: "pe-7s-note2",
+    component: TableList,
+    layout: ""
   },
-  // {
-  //   path: "/table",
-  //   name: "Table List",
-  //   icon: "pe-7s-note2",
-  //   component: TableList,
-  //   layout: ""
-  // },
-  // {
-  //   path: "/typography",
-  //   name: "Typography",
-  //   icon: "pe-7s-news-paper",
-  //   component: Typography,
-  //   layout: ""
-  // },
-  // {
-  //   path: "/icons",
-  //   name: "Icons",
-  //   icon: "pe-7s-science",
-  //   component: Icons,
-  //   layout: ""
-  // },
-  // {
-  //   path: "/maps",
-  //   name: "Maps",
-  //   icon: "pe-7s-map-marker",
-  //   component: Maps,
-  //   layout: ""
-  // },
-  // {
-  //   path: "/notifications",
-  //   name: "Notifications",
-  //   icon: "pe-7s-bell",
-  //   component: Notifications,
-  //   layout: ""
-  // }
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "pe-7s-bell",
+    component: Notifications,
+    layout: ""
+  }
 ];
 
 export default dashboardRoutes;
